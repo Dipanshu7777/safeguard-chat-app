b57c48f20b549feecfeb52a97fa4d4c7ea223c71
Dipanshu7777 <154437150+Dipanshu7777@users.noreply.github.com>
2025-08-13 00:51:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-You said:
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
 import { 
     getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, 
